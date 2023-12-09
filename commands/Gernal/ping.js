76dcd45461d264ad44d3151207c3f53e4b27cc97
ish0503 <98135486+ish0503@users.noreply.github.com>
2023-12-09 09:16:53 +0900

    Update ping.js

--- commands/Gernal/ping.js
+++ commands/Gernal/ping.js
@@ -1,29 +1,5 @@
 const { EmbedBuilder, SlashCommandBuilder } = require("discord.js")
 
-const imgs = [
-    "https://i2.ruliweb.com/img/23/06/05/18886eea7a1573866.jpg",
-    "https://i3.ruliweb.com/img/23/06/05/18886efdca2573866.jpg",
-    "https://i3.ruliweb.com/img/23/06/05/18886efae9e573866.jpg",
-    "https://i2.ruliweb.com/img/23/06/05/18886ef8039573866.jpg",
-    "https://i1.ruliweb.com/img/23/06/05/18886ef4e64573866.jpg",
-    "https://i1.ruliweb.com/img/23/06/05/18886eed4d9573866.jpg",
-    "https://i2.ruliweb.com/img/23/06/05/18886ee40de573866.jpg",
-    "https://i2.ruliweb.com/img/23/06/05/18886ee7855573866.jpg",
-    "https://i2.ruliweb.com/img/23/06/05/18886f079ad573866.jpg",
-]
-
-function getrandomImage(){
-    const min = 1
-    const max = 10
-    const randomNumber = Math.floor(Math.random() * (max - min))
-    if (imgs[randomNumber]){
-        return imgs[randomNumber]
-    }else {
-        return "https://i2.ruliweb.com/img/23/06/05/18886eea7a1573866.jpg"
-    }
-}
-
-
 module.exports = {
     data: new SlashCommandBuilder()
 
@@ -39,10 +15,10 @@ module.exports = {
         const msg = await interaction.fetchReply()
         
         const embed = new EmbedBuilder()
-        .setTitle(`🏓퐁! 응답이 왔어요!<:Heart:1151445619215441980>`)
+        .setTitle(`🏓퐁! 응답 도착!<:Heart:1151445619215441980>`)
         .setDescription(`🏓 커맨드 핑: ${msg.createdTimestamp - interaction.createdTimestamp}ms\n🏓 봇 핑 : ${interaction.client.ws.ping}ms`)
         .setColor(0xFFFF00)
 
-        interaction.editReply({ embeds: [embed],  files: [{ attachment: getrandomImage() }] })
+        interaction.editReply({ embeds: [embed] })
     }
 }
